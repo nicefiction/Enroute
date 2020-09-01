25293b9ad64eb7f16f0b3fa812cd68a7c2fa1786
Olivier Van hamme <nicefiction@gmail.com>
2020-09-01 22:43:36 +0200

    Creates the core date objects in the database .

--- Enroute/SceneDelegate.swift
+++ Enroute/SceneDelegate.swift
@@ -20,12 +20,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Get the managed object context from the shared persistent container.
-//        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext // 👋
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext // 👋
 
         // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
         // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
 //        let contentView = ContentView().environment(\.managedObjectContext, context) // 👋
         let contentView = FlightsEnrouteView(flightSearch: FlightSearch(destination: "KSFO"))
+            .environment(\.managedObjectContext , context)
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
